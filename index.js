63248055d44008c9ca9d6a35bf23a222688e4ed1
JonathanKosasih18 <jonathankosasih@gmail.com>
2025-05-04 19:22:18 +0700

    fixed cors config

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3001;
 const corsOptions = {
-    origin: "https://os.netlabdte.com",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
